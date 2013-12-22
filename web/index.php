c8eec7b09eb1a491f4aebce4d2185acba7e5b4a6
masted <masted311@gmail.com>
2013-12-22 16:11:16 +0400

    Commit was made from server /localhost by ci/push

--- web/index.php
+++ web/index.php
@@ -1,17 +1,14 @@
 <?php
 
-// ngn init
 define('NGN_ENV_PATH', dirname(dirname(__DIR__)));
 define('NGN_PATH', dirname(dirname(__DIR__)).'/ngn');
 
-// web init
 define('SITE_PATH', __DIR__.'/site');
 define('WEBROOT_PATH', __DIR__);
 define('IS_DEBUG', true);
 define('PROJECT_KEY', 'scripts');
 
-require_once NGN_PATH.'/init/core.php';
-require_once NGN_PATH.'/init/web.php';
+require_once NGN_PATH.'/init/site-web.php';
 
 define('SITE_TITLE', 'scripts');
 setConstant('SITE_LIB_PATH', SITE_PATH.'/lib');
