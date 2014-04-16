727f1abd320707e6723365edbfc2d58a59efb10c
masted <masted311@gmail.com>
2014-04-16 18:48:39 +0400

    Commit was made from server localhost by ci/push

--- copyDbRemote.php
+++ copyDbRemote.php
@@ -1,8 +1,8 @@
 <?php
 
 $c = include dirname(__DIR__).'/config/server.php';
-if (empty($_SERVER['argv'][1])) die('Db name not defined');
-if (empty($_SERVER['argv'][2])) die('Host not defined');
+if (empty($_SERVER['argv'][1])) die('Syntax: '.basename(__FILE__)." dbName host [port] [password]\n");
+if (empty($_SERVER['argv'][2])) die('Syntax: '.basename(__FILE__)." dbName host [port] [password]\n");
 $dbName = $_SERVER['argv'][1];
 $host = $_SERVER['argv'][2];
 $port = $_SERVER['argv'][3];
@@ -13,9 +13,6 @@ $p = "-u{$c['dbUser']} -p{$c['dbPass']}";
 if (isset($_SERVER['argv'][4])) $p2 = "-u{$c['dbUser']} -p{$_SERVER['argv'][4]}";
 else $p2 = $p;
 system("ssh user@$host -p $port '".
-  //"mysql $p -e \"DROP DATABASE IF EXISTS $dbName\"".
-  //"mysql $p -e \"CREATE DATABASE $dbName\"".
-  "mysql $p2 $dbName < /tmp/$dbName.sql; ".
   'mysql '.$p2.' -Nse "show tables" '.$dbName.' | while read table; do mysql '.$p2.' -e "drop table $table" '.$dbName.'; done; '. // delete all tables
   "mysql $p2 $dbName < /tmp/$dbName.sql; ".
   "rm /tmp/$dbName.sql; ".
