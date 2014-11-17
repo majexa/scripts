1789baa4fc512d9b2e61efa0ab2caf77bd10a9c0
masted <masted311@gmail.com>
2014-11-17 10:32:53 +0400

    Auto-commit from localhost

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 define('NGN_ENV_PATH', dirname(dirname(__DIR__)));
 define('NGN_PATH', dirname(dirname(__DIR__)).'/ngn');
 
-define('SITE_PATH', __DIR__.'/site');
+define('PROJECT_PATH', __DIR__.'/site');
 define('WEBROOT_PATH', __DIR__);
 define('IS_DEBUG', true);
 define('PROJECT_KEY', 'scripts');
@@ -11,7 +11,7 @@ define('PROJECT_KEY', 'scripts');
 require_once NGN_PATH.'/init/site-web.php';
 
 define('SITE_TITLE', 'scripts');
-setConstant('SITE_LIB_PATH', SITE_PATH.'/lib');
+setConstant('SITE_LIB_PATH', PROJECT_PATH.'/lib');
 Lib::addFolder(NGN_ENV_PATH.'/run/lib');
 
 print (new RouterScripts(['disableHeaders' => true]))->dispatch()->getOutput();
