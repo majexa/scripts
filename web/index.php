1e1ad16ee4b2affb9da619ebf563491c13d38575
masted <masted311@gmail.com>
2015-12-11 14:04:28 +0300

    Auto-commit on 11.12.2015 14:04:28

--- web/index.php
+++ web/index.php
@@ -1,17 +1,12 @@
 <?php
 
-define('NGN_ENV_PATH', dirname(dirname(__DIR__)));
-define('NGN_PATH', dirname(dirname(__DIR__)).'/ngn');
-
-define('PROJECT_PATH', __DIR__.'/site');
-define('WEBROOT_PATH', __DIR__);
-define('IS_DEBUG', true);
 define('PROJECT_KEY', 'scripts');
+define('WEBROOT_PATH', __DIR__);
 
-require_once NGN_PATH.'/init/site-web.php';
+require dirname(dirname(__DIR__)).'/ngn/init/web-standalone.php';
 
 define('SITE_TITLE', 'scripts');
-setConstant('SITE_LIB_PATH', PROJECT_PATH.'/lib');
+//setConstant('SITE_LIB_PATH', PROJECT_PATH.'/lib');
 Lib::addFolder(NGN_ENV_PATH.'/run/lib');
 
 print (new RouterScripts(['disableHeaders' => true]))->dispatch()->getOutput();
